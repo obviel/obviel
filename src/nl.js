5de1439c2643d4ebcb00bd9c25df069ab634ed0d
faassen <faassen@startifact.com>
2011-05-10 16:18:51 +0200

    basics for i18n support for forms.

---
+++ src/nl.js
@@ -0,0 +1,21 @@
+json_locale_data = {
+   "obviel_forms" : {
+      "" : {
+         "Plural-Forms" : " nplurals=2; plural=(n != 1);",
+         "MIME-Version" : " 1.0",
+         "POT-Creation-Date" : " 2011-05-03 22:54+0200",
+         "Last-Translator" : " Martijn Faassen <faassen@startifact.com>",
+         "Content-Type" : " text/plain; charset=ASCII",
+         "PO-Revision-Date" : " 2011-05-03 22:59+0200",
+         "Language-Team" : " Dutch",
+         "Content-Transfer-Encoding" : " 8bit",
+         "Project-Id-Version" : " PACKAGE VERSION",
+         "Report-Msgid-Bugs-To" : " "
+      },
+      "One field did not validate" : [
+         "%1 fields did not validate",
+         "Eén veld kon niet gevalideerd worden",
+         "%1 velden konden niet gevalideerd worden"
+      ]
+   }
+};
