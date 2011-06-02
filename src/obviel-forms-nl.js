a244daef6539b793d939d5eb699e5aecdfc166cd
faassen <faassen@startifact.com>
2011-06-02 15:56:45 +0200

    Better translation js file.

--- src/obviel-forms-nl.js
+++ src/obviel-forms-nl.js
@@ -1,1 +1,73 @@
-var json_locale_data = {"obviel_forms": {"": {"Content-Transfer-Encoding": "8bit", "Content-Type": "text/plain; charset=UTF-8", "Language-Team": "Dutch", "Last-Translator": "Martijn Faassen <faassen@startifact.com>", "MIME-Version": "1.0", "PO-Revision-Date": "2011-05-04 18:44+0200", "POT-Creation-Date": "2011-05-04 18:42+0200", "Plural-Forms": "nplurals=2; plural=(n != 1);", "Project-Id-Version": "obviel_forms 0.1", "Report-Msgid-Bugs-To": ""}, "1 field did not validate": ["%1 fields did not validate", "1 veld kon niet gevalideerd worden", "%1 velden konden niet gevalideerd worden"], "decimal may not contain more than %1 digits after the decimal mark": "er zijn niet meer dan %1 cijfers toegestaan na de komma", "decimal may not contain more than %1 digits before the decimal mark": "er zijn niet meer dan %1 cijfers toegestaan voor de komma", "decimal must contain at least %1 digits after the decimal mark": "er zijn tenminste %1 cijfers nodig na de komma", "decimal must contain at least %1 digits before the decimal mark": "er zijn tenminste %1 cijfers nodig voor de komma", "negative numbers are not allowed": "negatieve getallen zijn niet toegestaan", "not a decimal": "gebruik bij decimalen komma ipv punt", "not a float": "gebruik bij decimalen komma ipv punt", "not a number": "geen nummer", "not an integer number": "geen geheel getal", "this field is required": "dit veld is verplicht", "value must be %1 digits long": "de waarde moet %1 cijfers bevatten", "value too long": "invoer te veel karakters", "value too short": "invoer te weinig karakters"}};
+var json_locale_data = {
+    "obviel_forms": {
+        "": {
+            "Content-Transfer-Encoding": "8bit",
+            "Content-Type": "text/plain; charset=UTF-8",
+            "Language-Team": "Dutch",
+            "Last-Translator": "Martijn Faassen <faassen@startifact.com>",
+            "MIME-Version": "1.0",
+            "PO-Revision-Date": "2011-05-04 18:44+0200",
+            "POT-Creation-Date": "2011-05-04 18:42+0200",
+            "Plural-Forms": "nplurals=2; plural=(n != 1);",
+            "Project-Id-Version": "obviel_forms 0.1",
+            "Report-Msgid-Bugs-To": ""
+        },
+        "1 field did not validate": [
+            "%1 fields did not validate",
+            "1 veld kon niet gevalideerd worden",
+            "%1 velden konden niet gevalideerd worden"
+        ],
+        "decimal may not contain more than %1 digits after the decimal mark": [
+            null,
+            "er zijn niet meer dan %1 cijfers toegestaan na de komma"
+        ],
+        "decimal may not contain more than %1 digits before the decimal mark": [
+            null,
+            "er zijn niet meer dan %1 cijfers toegestaan voor de komma"
+        ],
+        "decimal must contain at least %1 digits after the decimal mark": [
+            null,
+            "er zijn tenminste %1 cijfers nodig na de komma"
+        ],
+        "decimal must contain at least %1 digits before the decimal mark": [
+            null,
+            "er zijn tenminste %1 cijfers nodig voor de komma"
+        ],
+        "negative numbers are not allowed": [
+            null,
+            "negatieve getallen zijn niet toegestaan"
+        ],
+        "not a decimal": [
+            null,
+            "gebruik bij decimalen komma ipv punt"
+        ],
+        "not a float": [
+            null,
+            "gebruik bij decimalen komma ipv punt"
+        ],
+        "not a number": [
+            null,
+            "geen nummer"
+        ],
+        "not an integer number": [
+            null,
+            "geen geheel getal"
+        ],
+        "this field is required": [
+            null,
+            "dit veld is verplicht"
+        ],
+        "value must be %1 digits long": [
+            null,
+            "de waarde moet %1 cijfers bevatten"
+        ],
+        "value too long": [
+            null,
+            "invoer te veel karakters"
+        ],
+        "value too short": [
+            null,
+            "invoer te weinig karakters"
+        ]
+    }
+};
