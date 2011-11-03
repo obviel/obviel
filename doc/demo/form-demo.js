aa64ffa9efbb9d7f4c807fbd7e0a24ecfff6ee50
faassen <faassen@startifact.com>
2011-11-03 16:20:23 +0100

    Include form demo.

--- doc/demo/form-demo.js
+++ doc/demo/form-demo.js
@@ -1,45 +1,37 @@
 
 $(document).ready(function() {
+    /* we start with an empty data object. If we
+       fill in values here, they will show up in the form */
     var data = {
-        'da': 'foo',
-        'dau': 'foo'
     };
 
+    /* we are going to render the form in the testform el */
     var el = $('#testform');
 
-
-    var orig_ajax = $.ajax;
-    $.ajax = function(settings) {
-        var key = settings.data.identifier || settings.data.term || '';
-        key = key.toLowerCase();
-        if ('foo'.indexOf(key) >= 0) {
-            settings.success([{value: 'foo', label: 'Foo'}]);
-        } else if ('bar'.indexOf(key) >= 0) {
-            settings.success([{value: 'bar', label: 'Bar'}]);
-        }
-    };
-    
+    /* here we render a viewform. We have included the form JSON
+       inline here, but of course you can produce this on the server
+       as well instead */
     el.render({
         ifaces: ['viewform'],
         form: {
             widgets: [
                 {
                     ifaces: ['boolean_field'],
-                    name: 'a',
-                    title: 'A',
+                    name: 'bool',
+                    title: 'boolean_field',
                     validate: {
                         required: true
                     }
                 },
                 {
                     ifaces: ['integer_field'],
-                    name: 'b',
-                    title: 'B'
+                    name: 'integer',
+                    title: 'integer_field'
                 },
                 {
                     ifaces: ['choice_field'],
-                    name: 'c',
-                    title: 'C',
+                    name: 'choice',
+                    title: 'choice_field',
                     choices: [
                         {value: 'foo', label: 'Foo'},
                         {value: 'bar', label: 'Bar'}
@@ -47,51 +39,68 @@ $(document).ready(function() {
                 },
                 {
                     ifaces: ['autocomplete_field'],
-                    name: 'da',
-                    title: 'Autocomplete',
+                    name: 'auto',
+                    title: 'autocomplete_field',
                     data: [
                         {value: 'foo', label: 'Foo'},
                         {value: 'bar', label: 'Bar'}
                     ],
                     defaultvalue: 'foo'
-                },
-                {
-                    ifaces: ['autocomplete_field'],
-                    name: 'dau',
-                    title: 'Autocomplete URL',
-                    data: 'autocomplete_url',
-                    defaultvalue: 'foo'
-                },
-               
+                },        
                 {
                     ifaces: ['datepicker_field'],
-                    name: 'dp',
-                    title: 'Datepicker'
+                    name: 'date',
+                    title: 'datepicker_field'
+                },
+                {
+                    ifaces: ['repeating_field'],
+                    name: 'repeating',
+                    title: 'repeating_field',
+                    widgets: [
+                        {
+                            ifaces: ['textline_field'],
+                            name: 'textline',
+                            title: 'textline_field'
+                        },{
+                            ifaces: ['text_field'],
+                            name: 'text',
+                            title: 'text_field'
+                        }]
                 }
-              
-
             ],
             controls: [
                 {
-                    'label': 'Examine',
+                    'label': 'Examine data',
                     'class': 'examine'
                 },
                 {
-                    'label': 'Change',
+                    'label': 'Change data',
                     'class': 'change'
                 }
-
             ]
         },
         data: data
     });
-
-    $('.examine', el).click(function(ev) {
-        console.log(data);
-    });
     
+    /* this will output the data object into the output blockquote element */
+    var render_data = function() {
+        var replacer = function(key, value) {
+            return value;
+        };
+        $('#output').text(JSON.stringify(data, replacer, 4));
+    };
+
+    /* show the underlying data object */
+    $('.examine', el).click(function(ev) {
+        render_data();
+    });
+
+    /* this will change the data object, the form will update
+       immediately */
     $('.change', el).click(function(ev) {
-        $(data).setField('da', 'bar');
+        $(data).setField('date', '2010-10-10');
+        $(data).setField('integer', 3);
+        render_data();
     });
     
 });
