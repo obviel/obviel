94e5b29116ab0837f79b9d579f0233638f20d3c7
faassen <faassen@startifact.com>
2011-11-03 14:52:44 +0100

    Configure things so that obviel demos can be included in the docs.

--- doc/conf.py
+++ doc/conf.py
@@ -120,7 +120,11 @@ html_theme_options = { "nosidebar": "true"}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static',
+                    '../test/dependencies', # obviel dependencies
+                    '../src', # obviel itself
+                    'demo', # demo code
+                    ]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
---
+++ doc/demo/form-demo.js
@@ -0,0 +1,97 @@
+
+$(document).ready(function() {
+    var data = {
+        'da': 'foo',
+        'dau': 'foo'
+    };
+
+    var el = $('#testform');
+
+
+    var orig_ajax = $.ajax;
+    $.ajax = function(settings) {
+        var key = settings.data.identifier || settings.data.term || '';
+        key = key.toLowerCase();
+        if ('foo'.indexOf(key) >= 0) {
+            settings.success([{value: 'foo', label: 'Foo'}]);
+        } else if ('bar'.indexOf(key) >= 0) {
+            settings.success([{value: 'bar', label: 'Bar'}]);
+        }
+    };
+    
+    el.render({
+        ifaces: ['viewform'],
+        form: {
+            widgets: [
+                {
+                    ifaces: ['boolean_field'],
+                    name: 'a',
+                    title: 'A',
+                    validate: {
+                        required: true
+                    }
+                },
+                {
+                    ifaces: ['integer_field'],
+                    name: 'b',
+                    title: 'B'
+                },
+                {
+                    ifaces: ['choice_field'],
+                    name: 'c',
+                    title: 'C',
+                    choices: [
+                        {value: 'foo', label: 'Foo'},
+                        {value: 'bar', label: 'Bar'}
+                        ]
+                },
+                {
+                    ifaces: ['autocomplete_field'],
+                    name: 'da',
+                    title: 'Autocomplete',
+                    data: [
+                        {value: 'foo', label: 'Foo'},
+                        {value: 'bar', label: 'Bar'}
+                    ],
+                    defaultvalue: 'foo'
+                },
+                {
+                    ifaces: ['autocomplete_field'],
+                    name: 'dau',
+                    title: 'Autocomplete URL',
+                    data: 'autocomplete_url',
+                    defaultvalue: 'foo'
+                },
+               
+                {
+                    ifaces: ['datepicker_field'],
+                    name: 'dp',
+                    title: 'Datepicker'
+                }
+              
+
+            ],
+            controls: [
+                {
+                    'label': 'Examine',
+                    'class': 'examine'
+                },
+                {
+                    'label': 'Change',
+                    'class': 'change'
+                }
+
+            ]
+        },
+        data: data
+    });
+
+    $('.examine', el).click(function(ev) {
+        console.log(data);
+    });
+    
+    $('.change', el).click(function(ev) {
+        $(data).setField('da', 'bar');
+    });
+    
+});
