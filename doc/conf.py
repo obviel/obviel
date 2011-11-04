95b416cdd887f6d5686141e2dfd307194b8fa288
faassen <faassen@startifact.com>
2011-11-04 20:06:54 +0100

    Big reorganization: everything is under 'src' now so that src can be easily included under the documentation. This makes it possible to easily publish the demos as part of the documentation while allowing the developers to hack on them without having to keep running sphinx all the time to regenerate the docs after making changes to JavaScript code.

--- doc/conf.py
+++ doc/conf.py
@@ -121,9 +121,7 @@ html_theme_options = { "nosidebar": "true"}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static',
-                    '../test/dependencies', # obviel dependencies
-                    '../src', # obviel itself
-                    'demo', # demo code
+                    '../src', # obviel itself & deps & demos
                     ]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
