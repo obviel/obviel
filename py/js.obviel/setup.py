4b367462ed6cae0c9b0fd09c45feebb6d78b785e
faassen <faassen@startifact.com>
2012-01-13 20:14:57 +0100

    Prepare for release.

--- py/js.obviel/setup.py
+++ py/js.obviel/setup.py
@@ -8,7 +8,7 @@ import os
 # For example, a packaging bugfix release version 1.4.4 of the
 # js.jquery package would be version 1.4.4-1 .
 
-version = '0.11-1dev'
+version = '0.11-1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
