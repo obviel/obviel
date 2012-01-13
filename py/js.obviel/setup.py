a86405babde90755e82883d87de87d9317499332
faassen <faassen@startifact.com>
2012-01-13 20:21:56 +0100

    Made a mistake.

--- py/js.obviel/setup.py
+++ py/js.obviel/setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-# The version of the wrapped library is the starting point for the
-# version number of the python package.
-# In bugfix releases of the python package, add a '-' suffix and an
-# incrementing integer.
-# For example, a packaging bugfix release version 1.4.4 of the
-# js.jquery package would be version 1.4.4-1 .
-
-version = '0.12dev'
+version = '0.12'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
