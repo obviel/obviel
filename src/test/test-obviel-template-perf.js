9e11568cd1b36e9395f1eb70fb95f72cedfe5572
faassen <faassen@startifact.com>
2012-05-16 17:33:54 +0200

    Some performance tests.

---
+++ src/test/test-obviel-template-perf.js
@@ -0,0 +1,62 @@
+/* performance benchmarks loosely based on these:
+
+http://genshi.edgewall.org/wiki/GenshiPerformance
+
+*/
+
+/*global module:false obviel:false test:false ok:false same:false $:false
+  equal:false raises:false asyncTest:false start:false deepEqual: false
+  stop:false  */
+
+module("Template Performance", {
+    setup: function() {
+        $('#jsview-area').html('<div id="viewdiv"></div>');
+    },
+    teardown: function() {
+
+    }
+});
+
+var module = obviel.template;
+
+/* it's hard to replicate a server-side test, because:
+
+* we have logic-less templates that don't involve calling any code
+  (in most areas)
+
+* we don't construct a full HTML page
+
+*/
+
+// // count starting with 1
+// // XXX special class in case 'last' is reached
+// // all really in the same tag with an each: tricky unless preprocess data!
+// var basic = module.Template(       
+// '<div>{user}</div>' +
+// '<div>{me}</div>' +
+// '<div>{world}</div>' +
+// '<h2>Loop</h2>' +
+// '<ul data-if="items">' +
+// '  <li data-each="items" class="@nr">{content}</li>' +
+// '</ul>');
+
+
+var bigtable = new module.Template(
+'<table>\n' +
+'<tr data-each="table">' +
+'<td data-each="@.">{@.}</td>' +
+'</tr>' +
+'</table>');
+
+var data = {
+    table: []    
+};
+
+for (var i = 0; i < 1000; i++) {
+    data.table.push([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+};
+
+test('big table', function() {
+    bigtable.render($('#viewdiv'), data);
+    expect(0);
+});
