a93707886bc51bac430ed4ecaf736b4d03f0eafc
faassen <faassen@startifact.com>
2012-05-31 13:40:41 +0200

    jsontemplate isn't referred to in these files.

--- src/obviel/obviel-forms-datepicker.js
+++ src/obviel/obviel-forms-datepicker.js
@@ -1,6 +1,6 @@
 /*global obviel: true, jQuery: true, template_url: true
   alert: true , browser: true, document: true, app_url: true,
-  window: true, jsontemplate: true
+  window: true
 */
 
 (function($, obviel, module) {
