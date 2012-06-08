b950da9d79757145ce908efb8be8a9da04f9deb3
faassen <faassen@startifact.com>
2012-06-08 16:50:59 +0200

    A first stab at obviel-i18n, a module that allows one to manage the locale and domain for translations.

---
+++ src/obviel/obviel-i18n.js
@@ -0,0 +1,146 @@
+// Obviel i18n support.
+// uses jsgettext. some of the logic is more hairy than it should
+// be as jsgettext for some reason decided to globally share all state
+// between Gettext objects...
+    
+if (typeof obviel === "undefined") {
+    var obviel = {};
+}
+
+obviel.i18n = {};
+
+var _ = null;
+
+(function($, module) {
+    var domains = {};
+
+    module.I18nError = function(message) {
+        this.message = message;
+    };
+
+    module.I18nError.prototype.toString = function() {
+        return 'I18nError: ' + this.message;
+    };
+    
+    module.create_translation_source = function(data) {
+        return function() {
+            var defer = $.Deferred();
+            defer.resolve(data);
+            return defer.promise();
+        };
+    };
+    
+    module.create_translation_source_from_url = function(url) {
+        return function() {
+            var defer = $.Deferred();
+            $.ajax({
+                type: 'GET',
+                url: url,
+                dataType: 'json',
+                success: function(obj) {
+                    defer.resolve(obj);
+                }
+            });
+            return defer.promise();
+        };
+    };
+
+    // a translation source that doesn't translate
+    // this is needed because just passing in {} as the translations
+    // will trip of jsgettext in thinking the domain cannot be found
+    module.create_empty_translation_source = function() {
+        return function() {
+            var defer = $.Deferred();
+            // make up a message id that will never occur in real life
+            defer.resolve({'!@#$%NEVEREVERTRANSLATED':
+                           [null, 'This translation will never happen']});
+            return defer.promise();
+        };
+    };
+    
+    // XXX can this be called to override translations for a particular domain?
+    module.register_translation = function(domain, locale, translation_source) {
+        var translations = domains[domain];
+        if (translations === undefined) {
+            translations = {};
+            domains[domain] = translations;
+        }
+        translations[locale] = translation_source;
+    };
+
+    module.clear_translations = function() {
+        domains = {};
+    };
+
+    var current_gt = new Gettext();
+    var current_locale = null;
+    var current_domain = null;
+    
+    module.clear_locale = function() {
+        // XXX goes into the insides of jsgettext...
+        Gettext._locale_data = undefined;
+        current_locale = null;
+        current_domain = null;
+    };
+    
+    module.set_locale = function(locale, domain) {
+        // bail out early if we have to do nothing
+        if (locale === current_locale && domain == current_domain) {
+            return;
+        }
+        if (locale === current_locale) {
+            module.set_domain(domain);
+            return;
+        }
+        current_locale = locale;
+        current_domain = domain;
+        
+        var locale_data = {};
+
+        var promise;
+        var promises = [];
+        for (d in domains) {
+            var translations = domains[d];
+            var translation_source = translations[locale];
+            if (translation_source === undefined) {
+                throw new module.I18nError("Unknown locale: " + locale);
+            }
+            // XXX use deferred for async loading?
+            promise = translation_source();
+            promise.done(function(translation_data) {
+                locale_data[d] = translation_data;
+            });
+            promises.push(promise);
+        }
+        var subviews_promise = $.when.apply(null, promises);
+        subviews_promise.done(function() {
+            // XXX really convince Gettext to forget about previous data
+            Gettext._locale_data = undefined;
+            if (domains[domain] === undefined) {
+                throw new module.I18nError("Unknown domain: " + domain);
+            }
+            current_gt = new Gettext({domain: domain,
+                                      locale_data: locale_data});
+        });
+    };
+
+    module.get_locale = function() {
+        return current_locale;
+    };
+    
+    module.get_domain = function() {
+        return current_domain;
+    };
+    
+    module.set_domain = function(domain) {
+        if (domains[domain] === undefined) {
+            throw new module.I18nError("Unknown domain: " + domain);
+        }
+        current_domain = domain;
+        current_gt.textdomain(domain);
+    };
+    
+    _ = function(msgid) {
+        return current_gt.gettext(msgid);
+    };
+}(jQuery, obviel.i18n));
---
+++ src/test/test-obviel-i18n.js
@@ -0,0 +1,278 @@
+/*global module:false obviel:false test:false ok:false same:false $:false
+  equal:false raises:false asyncTest:false start:false deepEqual: false
+  stop:false strictEqual:false */
+
+module("i18n", {
+    setup: function() {
+        
+    },
+    teardown: function() {
+        obviel.i18n.clear_translations();
+        obviel.i18n.clear_locale();
+    }
+});
+
+var i18n = obviel.i18n;
+
+var setup_translations = function() {
+    var en_US = i18n.create_empty_translation_source();
+    var fr_FR = i18n.create_translation_source({'Hello world!':
+                                                [null, 'Bonjour monde!']});
+    var nl_NL = i18n.create_translation_source({'Hello world!':
+                                                [null, 'Hallo wereld!']});
+    i18n.register_translation('i18ntest', 'en_US', en_US);
+    i18n.register_translation('i18ntest', 'fr_FR', fr_FR);
+    i18n.register_translation('i18ntest', 'nl_NL', nl_NL);
+};
+
+var setup_translations_multi_domains = function() {
+    var en_US = i18n.create_empty_translation_source();
+    var fr_FR = i18n.create_translation_source({'Hello world!':
+                                                [null, 'Bonjour monde!']});
+    var nl_NL = i18n.create_translation_source({'Hello world!':
+                                                [null, 'Hallo wereld!']});
+    i18n.register_translation('i18ntest', 'en_US', en_US);
+    i18n.register_translation('i18ntest', 'fr_FR', fr_FR);
+    i18n.register_translation('i18ntest', 'nl_NL', nl_NL);
+
+    // now register second domain called 'other'
+    en_US = i18n.create_empty_translation_source();
+    fr_FR = i18n.create_translation_source({'Bye world!':
+                                            [null, 'Au revoir monde!']});
+    nl_NL = i18n.create_translation_source({'Bye world!':
+                                            [null, 'Tot ziens wereld!']});
+
+    i18n.register_translation('other', 'en_US', en_US);
+    i18n.register_translation('other', 'fr_FR', fr_FR);
+    i18n.register_translation('other', 'nl_NL', nl_NL);
+};
+
+test('no locale set', function() {
+    setup_translations();
+    
+    equal(_('Hello world!'), 'Hello world!');
+});
+
+test('non-translating en_US locale', function() {
+    setup_translations();
+
+    i18n.set_locale('en_US', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Hello world!');
+});
+
+test('fr_FR locale', function() {
+    setup_translations();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+});
+
+test('switch locale from not set to fr_FR', function() {
+    setup_translations();
+
+    equal(_('Hello world!'), 'Hello world!');
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+});
+
+test('switch locale from fr_FR to not set', function() {
+    setup_translations();
+    
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+
+    i18n.clear_locale();
+    
+    equal(_('Hello world!'), 'Hello world!');
+});
+
+test('switch locale from non-translating en_US to translating fr_FR', function() {
+    setup_translations();
+
+    i18n.set_locale('en_US', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Hello world!');
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+});
+
+
+test('switch locale from translating fr_FR to non-translating en_EN', function() {
+    setup_translations();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+
+    i18n.set_locale('en_US', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Hello world!');
+});
+
+test('switch locale from translating fr_FR to translating nl_NL', function() {
+    setup_translations();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+
+    i18n.set_locale('nl_NL', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Hallo wereld!');
+});
+
+
+test('switch domain using set_domain, no locale set', function() {
+    setup_translations_multi_domains();
+
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Bye world!');
+
+    i18n.set_domain('other');
+
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Bye world!');
+});
+
+test('switch domain using set_domain, non-translating en_US locale', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('en_US', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Bye world!');
+
+    i18n.set_domain('other');
+
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Bye world!');
+});
+
+test('switch domain using set_domain, translating fr_Fr locale', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+
+    equal(_('Bye world!'), 'Bye world!');
+
+    i18n.set_domain('other');
+
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Au revoir monde!');
+});
+
+test('switch domain using set_locale, non-translating en_US locale', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('en_US', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Bye world!');
+
+    i18n.set_locale('en_US', 'other');
+    
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Bye world!');
+});
+
+test('switch domain using set_locale, translating fr_Fr locale', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    
+    equal(_('Hello world!'), 'Bonjour monde!');
+
+    equal(_('Bye world!'), 'Bye world!');
+
+    i18n.set_locale('fr_FR', 'other');
+    
+    equal(_('Hello world!'), 'Hello world!');
+
+    equal(_('Bye world!'), 'Au revoir monde!');
+});
+
+test('get_locale without locale set', function() {
+    equal(i18n.get_locale(), null);
+});
+
+test('get_domain without locale set', function() {
+    equal(i18n.get_domain(), null);
+});
+
+test('get_locale with locale set', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+
+    equal(i18n.get_locale(), 'fr_FR');
+});
+
+test('get_domain with locale set', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+
+    equal(i18n.get_domain(), 'i18ntest');
+});
+
+test('get_locale after locale change', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+    i18n.set_locale('nl_NL', 'i18ntest');
+    
+    equal(i18n.get_locale(), 'nl_NL');
+});
+
+test('get_domain after domain change', function() {
+    setup_translations_multi_domains();
+
+    i18n.set_locale('fr_FR', 'i18ntest');
+
+    i18n.set_domain('other');
+    equal(i18n.get_domain(), 'other');
+});
+
+test('use unknown locale', function() {
+    setup_translations();
+
+    raises(function() {
+        i18n.set_locale('unknown', 'bar');
+    }, i18n.I18nError);
+});
+
+test('use unknown domain in set_locale', function() {
+    setup_translations();
+
+    raises(function() {
+        i18n.set_locale('nl_NL', 'unknown');
+    }, i18n.I18nError);
+});
+
+test('use unknown domain in set_domain', function() {
+    setup_translations();
+
+    i18n.set_locale('nl_NL', 'i18ntest');
+    
+    raises(function() {
+        i18n.set_domain('unknown');
+    }, i18n.I18nError);
+});
+
+
