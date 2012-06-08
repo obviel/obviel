6920ac345d0f18fe589968fa86a6728048137b3d
faassen <faassen@startifact.com>
2012-06-08 17:34:31 +0200

    Update the demo to use the new i18n infrastructure.

--- src/demo/i18n-js-demo.js
+++ src/demo/i18n-js-demo.js
@@ -1,16 +1,15 @@
 (function() {
-    var locale_data = { 
-        'your_project_name': {
-            "Hello world!": [null, "Bonjour monde!"]
-        }
-    };
+    var fr_FR = obviel.i18n.create_translation_source(
+        {'Hello world!': 'Bonjour monde!'});
+    obviel.i18n.register_translation('app', 'fr_FR', fr_FR);
 
-    var gt = new Gettext({
-        domain: 'your_project_name',
-        locale_data: locale_data
+    $(document).ready(function() {
+        // will not be translated as no locale has yet been set
+        $('#untranslated').text(_("Hello world!"));
+        
+        // now set the locale and the domain
+        obviel.i18n.set_locale('fr_FR', 'app');
+        // this will be translated
+        $('#translated').text(_("Hello world!"));
     });
-
-    var _ = function(msgid) { return gt.gettext(msgid); };
-
-    alert(_("Hello world!"));
 }());
