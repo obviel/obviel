9efbaaaa279b254454ee66296e122c5e29f91a06
faassen <faassen@startifact.com>
2012-06-12 20:53:48 +0200

    Use new obviel.i18n API.

--- src/demo/i18n-js-demo.js
+++ src/demo/i18n-js-demo.js
@@ -1,14 +1,17 @@
 (function() {
-    var fr_FR = obviel.i18n.create_translation_source(
+    var fr_FR = obviel.i18n.translation_source(
         {'Hello world!': 'Bonjour monde!'});
-    obviel.i18n.register_translation('fr_FR', fr_FR, 'app');
+    obviel.i18n.register_translation('fr_FR', fr_FR);
 
-    $(document).ready(function() {
+    var _ = obviel.i18n.domain();
+    
+    $(document).ready(function() {    
         // will not be translated as no locale has yet been set
         $('#untranslated').text(_("Hello world!"));
         
-        // now set the locale and the domain
-        obviel.i18n.set_locale('fr_FR', 'app');
+        // now set the locale to French
+        obviel.i18n.set_locale('fr_FR');
+        
         // this will be translated
         $('#translated').text(_("Hello world!"));
     });
