2a402edc744b2f4755462e779f9c790f223c9272
faassen <faassen@startifact.com>
2012-07-20 16:10:19 +0200

    Better definition of dependencies.

--- buster.js
+++ buster.js
@@ -3,9 +3,16 @@ var config = module.exports;
 config["Obviel Tests"] = {
     rootPath: ".",
     environment: "browser",
+    libs: [
+        'src/dependencies/jquery.js',
+        'src/dependencies/jquery.mockjax.js',
+        'src/dependencies/Gettext.js'
+    ],
+    
     sources: [
-        "src/dependencies/*.js",
-        "src/obviel/*.js"
+        "src/obviel/obviel-i18n.js",
+        "src/obviel/obviel-template.js",
+        'src/obviel/obviel.js'
     ],
     tests: [
         "src/btest/test-*.js"
