f9c72b1e3f19691273ad3ee1c4e43b981985003e
faassen <faassen@startifact.com>
2012-07-31 15:56:48 +0200

    Port a few more tests. use sinonjs to do fake XHR instead of mockjax.

--- src/btest/test-obviel.js
+++ src/btest/test-obviel.js
@@ -13,16 +13,34 @@ var renderText = function() { this.el.text(this.obj.text); };
 
 var coreTestCase = buster.testCase("core tests", {
     setUp: function() {
-        
+        this.server = sinon.fakeServer.create();
+        this.server.autoRespond = true;
+
+        this.mockJson = function(url, json) {
+            var getResponse;
+            if ($.isFunction(json)) {
+                getResponse = json;
+            } else {
+                getResponse = function() {
+                    return json;
+                };
+            }
+            var response = function(request) { 
+                request.respond(200, { 'Content-Type': 'application/json'},
+                                JSON.stringify(getResponse()));
+            };
+            this.server.respondWith('GET', url, response);
+        };
     },
+
     tearDown: function() {
         obviel.clearRegistry();
         obviel.clearTemplateCache();
         obviel.i18n.clearTranslations();
         obviel.i18n.clearLocale();
         obviel.clearIface();
+        this.server.restore();
     },
-    
     'view with default name': function() {
         obviel.view({
             render: renderText
@@ -188,10 +206,132 @@ var coreTestCase = buster.testCase("core tests", {
             render: renderText
         });
         var el = testel();
+        this.server.restore();
         el.render(fixturePath + 'default.json').done(function() {
             assert.equals(el.text(), 'foo');
             done();
         });
+    },
+
+    'render url with name': function(done) {
+        obviel.view({
+            render: renderText,
+            name: 'foo'
+        });
+
+        var el = testel();
+
+        this.server.restore();
+        el.render(fixturePath + 'named.json', 'foo').done(function() {
+            assert.equals(el.text(), 'bar');
+            done();
+        });
+    },
+                                   
+    'render url with iface': function(done) {
+        obviel.view({
+            render: renderText,
+            iface: 'ifoo'
+        });
+
+        var el = testel();
+
+        this.server.restore();
+        el.render(fixturePath + 'interfaced.json').done(function() {
+            assert.equals(el.text(), 'baz');
+            done();
+        });
+    },
+
+    'render url with name and iface': function(done) {
+        obviel.view({
+            render: renderText,
+            iface: 'ifoo',
+            name: 'foo'
+        });
+
+        var el = testel();
+
+        this.server.restore();
+        el.render(fixturePath + 'named_interfaced.json', 'foo').done(
+            function() {
+                assert.equals(el.text(), 'qux');
+                done();
+            });
+    },
+
+    'rerender url': function(done) {
+        obviel.view({
+            iface: 'ifoo',
+            render: function() {
+                this.el.text(this.obj.text);
+            }
+        });
+        
+        var called = 0;
+        
+        this.mockJson('testUrl', function() {
+                called++;
+                return {ifaces: ['ifoo'],
+                        text: called.toString()};
+        });
+
+        var el = testel();
+        el.render('testUrl').done(function(view) {
+            assert.equals(view.el.text(), '1');
+            el.rerender().done(function(view) {
+                // this should call the URL again
+                assert.equals(view.el.text(), '2');
+                done();
+            });
+        });        
+    },
+    
+    'rerender context object': function() {
+        obviel.iface('rerender');
+            var numrenders = 0;
+        obviel.view({
+            iface: 'rerender',
+            render: function() {
+                var self = this;
+                numrenders++;
+                self.el.text(numrenders.toString());
+            }
+        });
+        
+        var el = testel();
+        el.render({ifaces: ['rerender']});
+        assert.equals(el.text(), '1');
+        el.rerender();
+        assert.equals(el.text(), '2');
+    },
+
+    'no content, empty render': function() {
+        obviel.view({
+            render: function() {}
+        });
+        var el = testel();
+        el.html('<span>foo</span>');
+        el.render({});
+        assert.equals(el.text(), 'foo');
+    },
+                                   
+    'no content no render': function() {
+        obviel.view({
+        });
+        var el = testel();
+        el.html('<span>foo</span>');
+        el.render({});
+        assert.equals(el.text(), 'foo');   
+    },
+
+
+    // XXX is this correct?
+    'rerender without viewstack': function() {
+            var newel = $('div');
+        // no checking, should just not throw an exception
+        $(newel).rerender();
+        assert(true);
     }
 
 });
