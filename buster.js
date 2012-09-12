376f090aa56e7c57223ab2719677d2edca737c3a
faassen <faassen@startifact.com>
2012-09-12 16:26:05 +0200

    Pull in jshashtable and jshashset so I don't have to roll my own.

--- buster.js
+++ buster.js
@@ -4,7 +4,9 @@ config["Obviel Tests"] = {
     rootPath: ".",
     environment: "browser",
     libs: [
-        'src/dependencies/jquery.js'
+        'src/dependencies/jquery.js',
+        'src/dependencies/jshashtable_src.js',
+        'src/dependencies/jshashset_src.js'
     ],
     sources: [
         'src/obviel/obviel-sync.js'
